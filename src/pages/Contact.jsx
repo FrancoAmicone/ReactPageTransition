b02bb0bf996cb64d5f3c8551ca93feb72ed478b5
FrancoAmicone <francoamicone1@gmail.com>
2025-03-07 21:03:06 -0300

    contact text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import Transition from "../components/Transition";
+import TextPressure from "../TextAnimations/TextPressure/TextPressure";
 
 const Contact = () => {
   return (
-    <div className="container">
-      <h1>Say Hello</h1>
+<div style={{
+      position: 'relative',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      zIndex: 1
+    }}>
+      <div style={{
+        marginLeft:'-200px',
+        width: '700px',
+        height: '100px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        pointerEvents: 'none'
+      }}>
+        <TextPressure
+          text="contact us now!"
+          flex={true}
+          alpha={false}
+          stroke={true}
+          width={true}
+          weight={true}
+          italic={false}
+          textColor="#000000"
+          strokeColor="#000000"
+          minFontSize={20}
+          fontFamily="Inter"
+        />
+      </div>
     </div>
   );
 };
